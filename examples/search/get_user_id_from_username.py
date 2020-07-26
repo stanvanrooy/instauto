76b2178e64e44e938e17d39746d6df9986ae4fa8
Stan van Rooy <stan@rooy.works>
2020-07-26 04:17:30 +0200

    add examples for converting username to user id

---
+++ examples/search/get_user_id_from_username.py
@@ -0,0 +1,17 @@
+from src.api.client import ApiClient
+import os
+
+from src.api.actions.structs.search import SearchUsername
+
+
+if __name__ == '__main__':
+    if os.path.isfile('./.instauto.save'):
+        client = ApiClient.initiate_from_file('./.instauto.save')
+    else:
+        client = ApiClient(user_name="your_username", password="your_password")
+        client.login()
+        client.save_to_disk('./.instauto.save')
+
+    s = SearchUsername.create("instapowr", 1)
+    resp = client.search_username(s).json()
+    user_id = resp['users'][0]['pk']

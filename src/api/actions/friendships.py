5182003385c31990a8798f8cab09df70a961475e
Stan van Rooy <stan@rooy.works>
2020-07-26 03:56:06 +0200

    add documentation to friendships endpoint

--- src/api/actions/friendships.py
+++ src/api/actions/friendships.py
@@ -21,19 +21,59 @@ class FriendshipsMixin:
         return self._request(f'friendships/{obj.endpoint}/{obj.user_id}/', Method.POST, data=as_d, signed=True)
 
     def follow_user(self, obj: CreateFriendship) -> Response:
+        """Follow a user"""
         return self._friendships_act(obj)
 
     def unfollow_user(self, obj: DestroyFriendship) -> Response:
+        """Unfollow a user"""
         return self._friendships_act(obj)
 
     def remove_follower(self, obj: RemoveFriendship) -> Response:
+        """Remove someone from your followers list, that is currently following you"""
         return self._friendships_act(obj)
 
     def show_follower(self, obj: ShowFriendship) -> Response:
+        """Retrieve information about a user"""
         # doesn't use _friendship_act, because it is a GET request.
         return self._request(f"friendships/{obj.endpoint}/{obj.user_id}/", Method.GET)
     
     def get_followers(self, obj: ShowFriendshipFollowers) -> Tuple[ShowFriendshipFollowers, Union[Response, bool]]:
+        """Retrieves the followers of an Instagram user. Examples of how to use can be found in
+        examples/friendships/get_followers.py.
+        Returns
+        ---------
+        ShowFriendshipFollowers
+            The object that was passed in as an argument, but with updated max_id and page attributes. DO NOT CHANGE
+            THOSE ATTRIBUTES.
+        Response || bool
+            Returns the response if users were retrieved, returns False if no more users are available. If there were
+            users available, the returned response will contain the following object:
+            ```json
+            {
+              "big_list": true,
+              "global_blacklist_sample": null,
+              "next_max_id": "QVFDN1BnZWpkTTF1SWdkaWVfVzVYbHNJV2NjemFlX2lWV0tjTUVvUGM2bTZrVHdpeGVnSVA4eXBCQVJ1MUVWeXZBNHBvRmtXa3VvWkFoS09PSG1rdVIwMg==",
+              "page_size": 200,
+              "sections": null,
+              "status": "ok",
+              "users": [
+                  {
+                    "account_badges": [],
+                    "full_name": "",
+                    "has_anonymous_profile_picture": false,
+                    "is_private": false,
+                    "is_verified": false,
+                    "latest_reel_media": 0,
+                    "pk": *********,
+                    "profile_pic_id": "*******",
+                    "profile_pic_url": "******",
+                    "username": "********"
+                  }
+                ]
+              }
+            }
+            ```
+        """
         if obj.max_id is None and obj.page > 0:
             return obj, False
         query_params = {
@@ -52,6 +92,41 @@ class FriendshipsMixin:
         return obj, resp
 
     def get_following(self, obj: ShowFriendshipFollowing) -> Tuple[ShowFriendshipFollowing, Union[Response, bool]]:
+        """Retrieves the following of an Instagram user. Examples of how to use can be found in
+        examples/friendships/get_following.py.
+        Returns
+        ---------
+        ShowFriendshipFollowers
+            The object that was passed in as an argument, but with updated max_id and page attributes. DO NOT CHANGE
+            THOSE ATTRIBUTES.
+        Response || bool
+            Returns the response if users were retrieved, returns False if no more users are available. If there were
+            users available, the returned response will contain the following object:
+            ```json
+            {
+              "big_list": true,
+              "global_blacklist_sample": null,
+              "next_max_id": "200",
+              "page_size": 200,
+              "sections": null,
+              "status": "ok",
+              "users": [
+                  {
+                    "account_badges": [],
+                    "full_name": "",
+                    "has_anonymous_profile_picture": false,
+                    "is_private": false,
+                    "is_verified": false,
+                    "latest_reel_media": 0,
+                    "pk": *******,
+                    "profile_pic_id": "*******",
+                    "profile_pic_url": "******",
+                    "username": "********"
+                  }
+                ]
+              }
+            ```
+        """
         if obj.max_id is None and obj.page > 0:  # max_id get's set to None when there are no more results.
             return obj, False
         query_params = {

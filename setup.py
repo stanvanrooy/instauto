f2d11bde4ce3895a5e5e0df810e162c25d8d0325
Stan van Rooy <stan@rooy.works>
2020-07-27 10:17:21 +0200

    update naming for imports

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@ import setuptools
 from distutils.core import setup
 
 setup(
-  name = 'instauto',         # How you named your package folder (MyLib)
-  packages = setuptools.find_packages(),   # Chose the same as "name"
-  version = '0.0.6',      # Start with a small number and increase it with every change you make
-  license='GNU GPLv3',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'Python wrapper for the private Instagram API',   # Give a short description about your library
-  author = 'Stan van Rooy',                   # Type in your name
-  author_email = 'stan@rooy.dev',      # Type in your E-Mail
-  url = 'https://github.com/stanvanrooy/instauto',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/stanvanrooy/instauto/archive/0.0.6.tar.gz',    # I explain this later on
-  keywords = ['instagram api', 'private instagram api'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
+  name = 'instauto',
+  packages = setuptools.find_packages(),
+  version = '0.0.8',
+  license='GNU GPLv3',
+  description = 'Python wrapper for the private Instagram API',
+  author = 'Stan van Rooy',
+  author_email = 'stan@rooy.dev',
+  url = 'https://github.com/stanvanrooy/instauto',
+  download_url = 'https://github.com/stanvanrooy/instauto/archive/0.0.7.tar.gz',
+  keywords = ['instagram api', 'private instagram api'],
+  install_requires=[
           'requests',
           'apscheduler',
           'pycryptodomex',

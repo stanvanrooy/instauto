f2ba2a93c208d0edbe588478d2918d7d247f4ada
Stan van Rooy <stan@rooy.dev>
2020-10-07 00:11:22 +0200

    Update for V0.0.16

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from distutils.core import setup
 setup(
   name='instauto',
   packages=setuptools.find_packages(),
-  version='0.0.15',
-  license='GNU GPLv3',
+  version='0.0.16',
+  license='MIT',
   description='Python wrapper for the private Instagram API',
   author='Stan van Rooy',
   author_email='stan@rooy.dev',
   url='https://github.com/stanvanrooy/instauto',
-  download_url='https://github.com/stanvanrooy/instauto/archive/0.0.15.tar.gz',
+  download_url='https://github.com/stanvanrooy/instauto/archive/0.0.16.tar.gz',
   keywords=['instagram api', 'private instagram api'],
   install_requires=[
           'requests',
@@ -24,5 +24,7 @@ setup(
     'Topic :: Software Development :: Libraries :: Python Modules',
     'License :: OSI Approved :: GNU General Public License v3 (GPLv3)  ',
     'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.6'
   ],
 )

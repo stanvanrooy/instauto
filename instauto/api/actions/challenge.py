caca72ddbecf9ad959ef6660209d34b73d325449
Stan van Rooy <stan@rooy.dev>
2020-10-07 00:02:56 +0200

    feat: add initial support for handling challenges

--- instauto/api/actions/challenge.py
+++ instauto/api/actions/challenge.py
@@ -25,36 +25,32 @@ class ChallengeMixin:
         if resp_data['message'] not in ('challenge_required', 'checkpoint_required'):
             raise BadResponse("Challenge required, but no URL provided.")
 
-        api_path = resp_data['challenge']['api_path'][1::]
-        resp = self._request(endpoint=api_path,
-                             method=Method.GET,
-                             query={
-                                 "guid": self.state.uuid,
-                                 "device_id": self.state.device_id
-                             })
-        bloks_action = resp_data['bloks_action']
-        # handle simple challenge
-        if bloks_action == 'com.instagram.challenge.navigation.take_challenge':
-            resp = self._request(
-                endpoint=api_path, method=Method.POST, data={
-                    "choice": 0,  # 0 = this was me, 1 = this wasn't me,
-                    "_csrftoken": self._session.cookies['csrftoken'],
-                    "_uuid": self.state.uuid,
-                    "bloks_versioning_id": self.state.bloks_version_id,
-                    "post": 1
-                }
-            )
-        elif True:
-            security_code = input("Type the security code here: ")
-            resp = self._request(
-                endpoint=api_path, method=Method.POST, data={
-                    "_csrftoken": self._session.cookies['csrftoken'],
-                    "_uuid": self.state.uuid,
-                    "bloks_versioning_id": self.state.bloks_version_id,
-                    "post": 1,
-                    "security_code": security_code
-                }
-            )
-            resp_data = resp.json()
-            self.state.logged_in_account_data = LoggedInAccountData(resp_data['logged_in_user'])
-        return False
+        api_path = resp_data['challenge']['api_path'][1:]
+        _ = self._request(endpoint=api_path, method=Method.GET, query={
+                                    "guid": self.state.uuid,
+                                    "device_id": self.state.device_id
+                                }
+                             )
+        # TODO: Add support for different kinds of challenges.
+        #       Currently, only the verification pin challenge is supported, and other challenges, such as the
+        #       'verify this was you', do not work.
+        _ = self._request(
+            endpoint=api_path, method=Method.POST, data={
+                "choice": 0,  # TODO: enum phone/email verification. Which value represent which one?
+                "_csrftoken": self._session.cookies['csrftoken'],
+                "_uuid": self.state.uuid,
+                "bloks_versioning_id": self.state.bloks_version_id,
+                "post": 1
+            }
+        )
+        security_code = input("Type the security code here: ")
+        _ = self._request(
+            endpoint=api_path, method=Method.POST, data={
+                "_csrftoken": self._session.cookies['csrftoken'],
+                "_uuid": self.state.uuid,
+                "bloks_versioning_id": self.state.bloks_version_id,
+                "post": 1,
+                "security_code": security_code
+            }
+        )
+        return True

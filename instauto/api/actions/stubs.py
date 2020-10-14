3a85741f7432e9d1c32784ffd9b969e2c081f801
Stan van Rooy <stan@rooy.dev>
2020-10-14 17:42:52 +0200

    fix: remove python3.8 feature (#77)
    Closes #74

--- instauto/api/actions/stubs.py
+++ instauto/api/actions/stubs.py
@@ -1,8 +1,8 @@
 from instauto.api.structs import Method
 import requests
-from typing import Protocol, Dict
+from typing import Dict
 
 
-class _request(Protocol):
+class _request:
     def __call__(self, endpoint: str, method: Method, query: dict = None, data: dict = None, headers: Dict[str, str]
     = None, default_headers: bool = None, signed: bool = None) -> requests.Response: ...

5b91801032374198b1c69d8eaf3be89162a79492
Jakub Juhas <juhas.jugi@gmail.com>
2020-11-04 09:44:59 +0100

    feat: get likers of media
    Closes #93.

---
+++ instauto/api/actions/tests/media.py
@@ -0,0 +1,14 @@
+import unittest
+
+from instauto.api.client import ApiClient
+import instauto.api.actions.post as ps
+from .common import helper
+
+
+class TestMedia(unittest.TestCase):
+
+    def test_media_likers(self):
+        client = ApiClient(testing=True)
+        s = ps.RetrieveLikers()
+        s.fill(client)
+        helper(s, self)

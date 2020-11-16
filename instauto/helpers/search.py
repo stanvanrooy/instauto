1c5bb94141f4f60441e869a20ae09f6abeccbe62
Stan van Rooy <stan@rooy.dev>
2020-11-17 00:09:28 +0100

    feat: add helper methods for (almost?) all actions. (#101)
    Closes #23, closes #26

---
+++ instauto/helpers/search.py
@@ -0,0 +1,30 @@
+from typing import List
+
+from instauto.api.client import ApiClient
+from instauto.api.actions import search as se
+
+
+def search_username(client: ApiClient, username, count: int) -> List[dict]:
+    username = se.Username(
+        q=username,
+        count=count
+    )
+    resp = client.search_username(username)
+    return resp.json()['users']
+
+
+def get_user_by_username(client: ApiClient, username: str) -> dict:
+    users = search_username(client, username, 1)
+    if users[0]['username'] == username:
+        return users[0]
+
+
+def get_user_id_from_username(client: ApiClient, username: str):
+    user = get_user_by_username(client, username)
+    return user['pk']
+
+
+def search_tags(client: ApiClient, tag: str, limit: int) -> List[dict]:
+    s = se.Tag(tag, limit)
+    resp: dict = client.search_tag(s).json()
+    return resp['results']

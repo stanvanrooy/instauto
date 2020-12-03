1e5dc7896612f89e860a63d1c17ab0b5e461f444
Stan van Rooy <stan@rooy.dev>
2020-12-04 00:44:02 +0100

    chore: add example for just retrieving your inbox

---
+++ examples/direct/direct_get_inbox.py
@@ -0,0 +1,13 @@
+import os
+import json
+from instauto.api.client import ApiClient
+
+if __name__ == '__main__':
+    if os.path.isfile('./.instauto.save'):
+        client = ApiClient.initiate_from_file('./.instauto.save')
+    else:
+        client = ApiClient(user_name=os.environ.get("INSTAUTO_USER") or "your_username", password=os.environ.get("INSTAUTO_PASS") or "your_password")
+        client.login()
+        client.save_to_disk('./.instauto.save')
+
+    client.direct_get_inbox()

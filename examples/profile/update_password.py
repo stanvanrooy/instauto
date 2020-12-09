c3cf2c04467e4d2e305128067f529d262e231d5e
Stan van Rooy <stan@rooy.dev>
2020-12-09 23:09:58 +0100

    docs: add example for changing password

---
+++ examples/profile/update_password.py
@@ -0,0 +1,14 @@
+import os
+
+from instauto.api.actions import profile as pr
+from instauto.api.client import ApiClient
+
+if __name__ == '__main__':
+    if os.path.isfile('./.instauto.save'):
+        client = ApiClient.initiate_from_file('./.instauto.save')
+    else:
+        client = ApiClient(user_name=os.environ.get("INSTAUTO_USER") or "your_username", password=os.environ.get("INSTAUTO_PASS") or "your_password")
+        client.login()
+        client.save_to_disk('./.instauto.save')
+
+    client.change_password("new_password", "old_password")

affa533442eb89a6b3d6082e0cd05039affb02ea
Stan van Rooy <stan@rooy.dev>
2021-08-28 21:30:34 +0200

    release: setup for release 2.0.6

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Stan van Rooy'
 author = 'Stan van Rooy'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.3'
+release = '2.0.6'
 
 
 # -- General configuration ---------------------------------------------------
